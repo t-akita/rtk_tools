eef335e087ea7f487fa7bb594e7a55f07b41bff7
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2019-08-22 15:34:53 +0900

    two stage yaml loader

--- script/dashboard.py
+++ script/dashboard.py
@@ -22,7 +22,8 @@ from tkfilebrowser import askopendirname, askopenfilenames, asksaveasfilename
 from rtk_tools import dictlib
 
 Config={
-  "path":"~/",
+  "packpath":"~/",
+  "load":"",
   "confirm":"Stop anyway",
   "autoclose":10,
   "altitude":"+0",
@@ -63,10 +64,10 @@ def cb_load(msg):
   Param["recipe"]=msg.data
   wRecipe.delete(0,tk.END)
   wRecipe.insert(0,Param["recipe"])
-  if os.system("ls "+Config["path"]+"/recipe.d/"+Param["recipe"])==0:
+  if os.system("ls "+packpath+"/recipe.d/"+Param["recipe"])==0:
     rospy.set_param("/dashboard",Param)
-    commands.getoutput("cd "+Config["path"]+"; rm recipe; ln -s recipe.d/"+Param["recipe"]+" recipe")
-    commands.getoutput("rosparam load "+Config["path"]+"/recipe/param.yaml")
+    commands.getoutput("cd "+packpath+"; rm recipe; ln -s recipe.d/"+Param["recipe"]+" recipe")
+    commands.getoutput("rosparam load "+packpath+"/recipe/param.yaml")
     res=Bool(); res.data=True; pub_Y3.publish(res)
     pub_msg.publish("recipe_manager::cb_load "+Param["recipe"])
   else:
@@ -79,7 +80,7 @@ def cb_open_dir():
   msgBox=tk.Tk()
   msgBox.title("Open dir")
   msgBoxWait=msgBox.after(Config["autoclose"]*1000,cb_autoclose)
-  ret=askopendirname(parent=msgBox,initialdir=Config["path"]+"/recipe.d",initialfile="")
+  ret=askopendirname(parent=msgBox,initialdir=packpath+"/recipe.d",initialfile="")
   if msgBoxWait is None: return
   msgBox.after_cancel(msgBoxWait)
   msgBoxWait=None
@@ -93,7 +94,7 @@ def cb_open_dir():
 def cb_save():
   global msgBox
   if msgBox is not None: return
-  ret=asksaveasfilename(parent=root,defaultext="",initialdir=Config["path"]+"/recipe.d",initialfile="",filetypes=[("Directory", "*/")])
+  ret=asksaveasfilename(parent=root,defaultext="",initialdir=packpath+"/recipe.d",initialfile="",filetypes=[("Directory", "*/")])
   if ret != "":
     print "save",ret
 
@@ -231,36 +232,32 @@ def cb_log():
   text.insert("1.0",msg)
 
 ########################################################
-def parse_argv(argv):
-  args={}
-  for arg in argv:
-    tokens = arg.split(":=")
-    if len(tokens) == 2:
-      key = tokens[0]
-      args[key] = tokens[1]
-  return args
-########################################################
 rospy.init_node("dashboard",anonymous=True)
 try:
   dictlib.merge(Config,rospy.get_param("/config/dashboard"))
 except Exception as e:
   print "get_param exception:",e.args
-dictlib.merge(Config,parse_argv(sys.argv))
+packpath=commands.getoutput("rospack find "+Config["package"])
+commands.getoutput("rosparam load "+packpath+"/dashboard.d/"+Config["load"])
+try:
+  dictlib.merge(Config,rospy.get_param("/config/dashboard"))
+except Exception as e:
+  print "get_param exception:",e.args
 
 ####sub pub
 rospy.Subscriber("~load",String,cb_load)
 rospy.Subscriber("/message",String,cb_sub)
 pub_Y3=rospy.Publisher("~loaded",Bool,queue_size=1)
 pub_E3=rospy.Publisher("~failed",Bool,queue_size=1)
+pub_msg=rospy.Publisher("/message",String,queue_size=1)
 
 ####reflect file link to parameter server
-if "path" in Config:
-  ln=commands.getoutput("ls -l "+Config["path"]+"/recipe")
-  if "->" in ln:
-    dst=re.sub(r".*->","",ln)
-    Param["recipe"]=re.sub(r".*/","",dst)
-    rospy.set_param("/dashboard",Param)
-  commands.getoutput("rosparam load "+Config["path"]+"/recipe/param.yaml")
+ln=commands.getoutput("ls -l "+packpath+"/recipe")
+if "->" in ln:
+  dst=re.sub(r".*->","",ln)
+  Param["recipe"]=re.sub(r".*/","",dst)
+  rospy.set_param("/dashboard",Param)
+commands.getoutput("rosparam load "+packpath+"/recipe/param.yaml")
 
 ####Layout####
 normalfont=(Config["font"]["family"],Config["font"]["size"],"normal")
